fb6cc1a88115998915930d6c07762bddf535089f
Greg Priday <greg@siteorigin.com>
2024-03-10 14:14:02 +0200

    Fixed PHPDoc block

--- src/Facades/Serper.php
+++ src/Facades/Serper.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \GregPriday\LaravelSerper\Serper
  *
- * @method static \GregPriday\LaravelSerper\Response\SerperResults search(string $query, array $params = [])
- * @method static \GregPriday\LaravelSerper\Response\SerperResults[] searchMulti(array $queries, int $results = 10, string $type = 'search', array $params = [])
+ * @method static \GregPriday\LaravelSerper\Response\SerperResults|\GregPriday\LaravelSerper\Response\SerperNewsResults search(string $query, int $count = 10, string $type = 'search')
+ * @method static \GregPriday\LaravelSerper\Response\SerperResults[] searchMulti(array $queries, int $count = 10, string $type = 'search')
  */
 class Serper extends Facade
 {
